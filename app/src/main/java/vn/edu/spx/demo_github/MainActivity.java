f39358cbda503b188665efe022a19f2b45f8d900
Dell <thienleu02@gmail.com>
2022-11-11 15:37:59 +0700

    thien da them mot thong bao

--- app/src/main/java/vn/edu/spx/demo_github/MainActivity.java
+++ app/src/main/java/vn/edu/spx/demo_github/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Đây là phần thêm", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Thien da them dong nay", Toast.LENGTH_SHORT).show();
     }
 }
